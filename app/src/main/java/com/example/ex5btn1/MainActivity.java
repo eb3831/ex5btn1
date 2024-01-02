481b7977443e0f3a74e11baa6cbe33a28feba032
eb3831 <eb3831@bs.amalnet.k12.il>
2024-01-02 09:09:31 +0200

    update 2

--- app/src/main/java/com/example/ex5btn1/MainActivity.java
+++ app/src/main/java/com/example/ex5btn1/MainActivity.java
@@ -11,7 +11,6 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     Button btn;
 
-
     TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
